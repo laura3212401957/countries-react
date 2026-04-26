2eaff0ad54bb64122c488dc445e7ad3eda571d9c
Laura V Cardona <58284758+laura3212401957@users.noreply.github.com>
2026-04-26 04:03:12 +0000

    Modificaciones

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 import Original from './Original';
 import Lista from './Lista';
 
 export default function App() {
+
+  const [pantalla, setPantalla] = useState('original');
+
+  const renderPantalla = () => {
+    if (pantalla === 'original') return <Original />;
+    if (pantalla === 'lista') return <Lista />;
+  };
+
   return (
-    <View style={styles.container}>
+ <View style={styles.container}>
       <StatusBar style="auto" />
       <Original/>
       <Lista/>
     </View>
+
 
   );
 }
